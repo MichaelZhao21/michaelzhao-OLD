e998e71faf3dfd03b6ee534f325dd48058a63451
MichaelZhao21 <michaelzhao314@gmail.com>
2020-05-22 15:36:20 -0400

    Started about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './About.scss';
 
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
 class About extends React.Component {
 	render() {
 		return (
-			<div>
-				This is the about page!
-			</div>
+			<Container fluid>
+				<h1>About Me!</h1>
+				<Jumbotron className="intro">
+					<p>Hey! My name is Michael Zhao and I am cool hehe :DD</p>
+				</Jumbotron>
+			</Container>
 		)
 	}
 }
