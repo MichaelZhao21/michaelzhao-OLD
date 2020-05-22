40ed7602feb01193ea4c1054fe3458c1c308ef6f
MichaelZhao21 <michaelzhao314@gmail.com>
2020-05-22 16:01:28 -0400

    moved blog_files to src & basic react-markdown!

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Blog.scss';
+import path from '../blog_files/website-creation.md';
+
+import ReactMarkdown from "react-markdown";
 
 class Blog extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {markdown:null}
+	}
+
+	componentWillMount() {
+		fetch(path).then(response => response.text()).then(text => {
+			this.setState({markdown: text})
+		});
+	}
+
 	render() {
 		return (
-			<div>
-				Blog content here :O
+			<div className="blog-content">
+				<ReactMarkdown source={this.state.markdown} />
 			</div>
 		)
 	}
