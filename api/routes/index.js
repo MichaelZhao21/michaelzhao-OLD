694db3a1f819a00433cf6571fb61456930817a5a
Ubuntu <ubuntu@ip-172-31-62-232.ec2.internal>
2020-05-26 23:24:54 +0000

    api subdomain works!

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("HI GRACE AHAHAAH!");
 });
 
 module.exports = router;
