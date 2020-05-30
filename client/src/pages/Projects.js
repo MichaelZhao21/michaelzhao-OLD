d23a0040fcd2263bef908d0ccae57a74d6d85568
Ubuntu <ubuntu@ip-172-31-62-232.ec2.internal>
2020-05-30 07:24:25 +0000

    Test scene

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './Projects.scss';
+import Scene from '../matter/Scene.js';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 class Projects extends React.Component {
 	render() {
 		return (
 			<div>
-				<Jumbotron className="temp">
-					<h1>Site under construction!</h1>
-				</Jumbotron>
+				<Scene />
 			</div>
 		)
 	}
