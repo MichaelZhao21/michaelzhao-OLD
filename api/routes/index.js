a5e0e191bd27454f9515ad7e44f60e5df420f39f
Ubuntu <ubuntu@ip-172-31-62-232.ec2.internal>
2020-05-30 03:20:19 +0000

    added test api and matter-js

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("HI GRACE AHAHAAH!");
+  res.send("Hi UwU");
+});
+
+router.get('/hotdude', function(req, res, next) {
+  res.sendFile(__dirname + '/ttt.png');
 });
 
 module.exports = router;
