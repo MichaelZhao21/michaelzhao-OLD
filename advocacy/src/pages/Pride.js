2ddf6977f58ff4b5e97de7335d6b98c4789bed02
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-04 19:38:33 -0400

    update study + pride

--- advocacy/src/pages/Pride.js
+++ advocacy/src/pages/Pride.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './style.css'
 import './css/Pride.css'
 import Footer from './Footer';
+import pridefall from '../files/pridefall-4chan-post.jpg';
 
 class Pride extends React.Component {
     render() {
@@ -12,7 +13,8 @@ class Pride extends React.Component {
                     <h1>Pride and Pridefall</h1>
                 </div>
                 <div className="container">
-                    <p>Pride and Pridefall</p>
+                    <h2>Pridefall</h2>
+                    <img className="doc-img drop-img" src={pridefall}></img>
                     <Footer credit="Mercedes Mehling on Unsplash" link="https://unsplash.com/photos/7J7x8HLXQKA"></Footer>
                 </div>
             </div>
