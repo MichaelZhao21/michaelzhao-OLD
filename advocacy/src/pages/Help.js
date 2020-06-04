3e29a876a30e56134639c446bc256b7c99d6ff25
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-04 04:33:22 -0400

    Addded insta post

--- advocacy/src/pages/Help.js
+++ advocacy/src/pages/Help.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import InstagramEmbed from 'react-instagram-embed';
 
 import './style.css';
 import './css/Help.css';
@@ -13,7 +14,7 @@ class Help extends React.Component {
                 </div>
                 <div class="container">
                     <h2>Petitions to Sign</h2>
-                    
+
                     <h3>Change.org</h3>
                     <a className="link link-list" href="https://www.change.org/p/mayor-jacob-frey-justice-for-george-floyd?source_location=petitions_browse">Justice for George Floyd</a>
                     <a className="link link-list" href="https://www.change.org/p/andy-beshear-justice-for-breonna-taylor?source_location=petitions_browse">Justice for Breonna Taylor</a>
@@ -21,7 +22,7 @@ class Help extends React.Component {
                     <a className="link link-list" href="https://www.change.org/p/ventilator-manufacturers-remove-barriers-to-fixing-ventilators?source_location=petitions_browse">Remove barriers to fixing ventilators</a>
                     <a className="link link-list" href="https://www.change.org/p/minneapolis-district-attorney-raise-the-degree?source_location=petitions_browse">RAISE THE DEGREE (Of Derek Chauvin's Murder Conviction)</a>
                     <a className="link link-list" href="https://www.change.org/p/change-org-the-minneapolis-police-officers-to-be-charged-for-murder-after-killing-innocent-black-man?source_location=petitions_browse">Charge the Other Three Officers</a>
-                    
+
                     <h3>Others</h3>
                     <a className="link link-list" href="https://blacklivesmatter.com/defundthepolice/">BLM: #Defund the Police</a>
                     <a className="link link-list" href="https://www.naacp.org/campaigns/we-are-done-dying/">NAACP: We are Done Dying Campagin</a>
@@ -42,6 +43,7 @@ class Help extends React.Component {
                     <a className="link link-list" href="https://www.gofundme.com/f/justice-for-regis">Justice For Regis (gofundme)</a>
 
                     <h2>Going to Protest</h2>
+                    <InstagramEmbed url='https://instagr.am/p/CA0jzCdg_vR' maxWidth={800} hideCaption={false} containerTagName='div'></InstagramEmbed>
 
                     <h2>Support</h2>
                     <a className="link link-list" href="https://docs.google.com/document/d/1qpn5fFrG4-FLfGo3cUsiwb7OvDnnL10uOnF50X-1v90/edit">List of Black-Owned Businesses</a>
@@ -49,7 +51,7 @@ class Help extends React.Component {
                     <div className="spacer"></div>
                     <Footer credit="Perry Grone on Unsplash" link="https://unsplash.com/photos/lbLgFFlADrY"></Footer>
                 </div>
-            </div>
+            </div >
         )
     }
 }
