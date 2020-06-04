a1e0dd9bf08d43972c9ccebde3ef17793b9754a1
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-04 05:34:07 -0400

    Update ext. resources desc

--- advocacy/src/pages/External.js
+++ advocacy/src/pages/External.js
@@ -12,7 +12,8 @@ class External extends React.Component {
                     <h1>External Resources</h1>
                 </div>
                 <div class="container">
-                    <p>External Resources</p>
+                    <div className="spacer"></div>
+                    <p>This section will just be a compilation of good social media posts to read/watch/listen to. Again, remember that there is ALWAYS a bias in any source of information, especially through social media. However, when taken with a grain of salt, the information from social media can be extremely precious and helps with the spread of activism and change.</p>
                     <Footer credit="dole777 on Unsplash" link="https://unsplash.com/photos/EQSPI11rf68"></Footer>
                 </div>
             </div>
