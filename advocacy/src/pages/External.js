8c8c09ff6daab8fd0b44e95403b990898b147f15
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-05 12:04:51 -0400

    tiktok?

--- advocacy/src/pages/External.js
+++ advocacy/src/pages/External.js
@@ -14,6 +14,17 @@ class External extends React.Component {
                 <div className="container">
                     <div className="spacer"></div>
                     <p>This section will just be a compilation of good social media posts to read/watch/listen to. Again, remember that there is ALWAYS a bias in any source of information, especially through social media. However, when taken with a grain of salt, the information from social media can be extremely precious and helps with the spread of activism and change.</p>
+
+                    <blockquote className="tiktok-embed" cite="https://www.tiktok.com/@goddessofegypt/video/6832014476825414918" data-video-id="6832014476825414918">
+                        <section>
+                            <a target="_blank" title="@goddessofegypt" href="https://www.tiktok.com/@goddessofegypt">@goddessofegypt</a>
+                            <p>
+                                yes, they were all black. SAY their names. LEARN their stories. FIGHT for a change. <a title="blmmovement" target="_blank" href="https://www.tiktok.com/tag/blmmovement">##blmmovement</a> <a title="blacklivesmatter" target="_blank" href="https://www.tiktok.com/tag/blacklivesmatter">##blacklivesmatter</a> <a title="heard" target="_blank" href="https://www.tiktok.com/tag/heard">##heard</a>
+                            </p>
+                            <a target="_blank" title="♬ Black Lives will be heard - goddessofegypt" href="https://www.tiktok.com/music/Black-Lives-will-be-heard-6832014470328486662">♬ Black Lives will be heard - goddessofegypt</a>
+                        </section>
+                    </blockquote>
+
                     <Footer credit="dole777 on Unsplash" link="https://unsplash.com/photos/EQSPI11rf68"></Footer>
                 </div>
             </div>
--- advocacy/src/pages/WhitePrivilege.js
+++ advocacy/src/pages/WhitePrivilege.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './css/WhitePrivilege.css';
 import './style.css'
 import Footer from './Footer';
+import text from '../files/text.json';
 
 class WhitePrivilege extends React.Component {
     render() {
@@ -11,11 +12,16 @@ class WhitePrivilege extends React.Component {
                 <div className="header white-header">
                     <h1>White Privilege</h1>
                 </div>
-                <div clasNames="container">
-                    <h2>TRUMP</h2>
+                <div className="container">
+                    <div className="spacer"></div>
+                    <h2>Trump's Photoshoot</h2>
                     <div className="video">
                         <iframe className="nyt-vid" title="New York Times Video - Embed Player" frameborder="0" scrolling="no" allowfullscreen="true" marginheight="0" marginwidth="0" id="nyt_video_player" src="https://www.nytimes.com/video/players/offsite/index.html?videoId=100000007168615"></iframe>
                     </div>
+                    <p className="p">{text.white[0]}</p>
+
+                    <h2>Sources for this Section</h2>
+                    <p className="citation">{text.white[1]}</p>
                     <Footer credit="WABE: Where ATL meets NPR" link="https://www.wabe.org/he-did-not-pray-fallout-grows-from-trump-s-photo-op-at-st-john-s-church/"></Footer>
                 </div>
             </div>
