ab6cccf28bfaa552903036cc773fd38581721521
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-20 02:31:16 -0400

    Added postup api path

--- api/app.js
+++ api/app.js
@@ -4,9 +4,11 @@ var app = express();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
+var postUpRouter = require('./routes/postUp');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/testAPI', testAPIRouter);
+app.use('/postup', postUpRouter);
 
 app.listen(8080, '172.31.62.232');
