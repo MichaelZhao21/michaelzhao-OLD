dd053ac4678397ea049c03602709817170d2e23f
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-21 01:55:38 -0400

    Added working vision :D

--- api/routes/postUp.js
+++ api/routes/postUp.js
@@ -7,11 +7,22 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/vision', function(req, res, next) {
-    var x = vision();
+    vision(res);
 })
 
-function vision() {
-    
-}
+async function vision(res) {
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+  
+    var pic = 'https://api.michaelzhao.xyz/hotdude';
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+  
+    // Performs label detection on the image file
+    const [result] = await client.labelDetection(pic);
+    const labels = result.labelAnnotations;
+    res.send(labels);
+}  
 
 module.exports = router;
