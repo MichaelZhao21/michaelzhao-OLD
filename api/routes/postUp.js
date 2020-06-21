64ddc6e39370b97e1ced02f16977f890e4df8eb0
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-21 08:26:01 -0400

    test18

--- api/routes/postUp.js
+++ api/routes/postUp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var path = require('path');
 
 router.get('/', function(req, res, next) {
     res.send("UwU Mikey is HERE YASSSSS");
