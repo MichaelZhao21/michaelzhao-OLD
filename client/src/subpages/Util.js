80d5b0c8e8e5c31c4d2d339b8e6781b8f072614e
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-30 14:29:56 -0500

    Util class added

---
+++ client/src/subpages/Util.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+export const ChangeIcon = ({props}) => {
+    console.log(props);
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = props;
+    document.getElementsByTagName('head')[0].appendChild(link);
+    return null;
+}
