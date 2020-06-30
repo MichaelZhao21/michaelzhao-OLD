002e2dcfa58db2d86c09e2caf6d0ffc9ee621bbc
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-30 14:40:06 -0500

    Fixed about

--- client/src/subpages/about/About.js
+++ client/src/subpages/about/About.js
@@ -4,7 +4,7 @@ import './About.scss';
 class About extends React.Component {
 	render() {
 		return (
-			<div className="custom-container">
+			<div className="about">
 				<h1 className="about-title">About Me!</h1>
 				<div className="custom-container about-intro">
 					<p>Hey! My name is Michael Zhao, and I am going to be a senior at the <a href="https://tams.unt.edu/">Texas Academy of Mathematics and Science</a>! I love making and building things like this website! In addition to web design and programming, I also really like art. I've done a lot of sketching, but recently I've gotten into painting and even painted a mural on my wall (See projects soon). I also enjoy cooking and making all sorts of creative foods from traditional Chinese dishes to an entire roast turkey! I love hanging out with my friends and meeting new people. I'm always free to talk; just hit me up on one of my socials. Love you all! :DDD</p>
